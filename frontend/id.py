75e8b308c7769f2198f6113b257e03dcfe71ad22
Penkich <nishijim2001@yahoo.co.jp>
2016-09-25 13:21:18 +0900

    Update id.py

--- frontend/id.py
+++ frontend/id.py
@@ -9,8 +9,8 @@ import nfc
 import re
  
 def connected(tag):
-  # タグのIDなどを出力する
-  #  print tag
+    # タグのIDなどを出力する
+    #  print tag
     a = '%s' % tag
     print re.findall("ID=([0-9A-F]*)",a)[0]
 # タッチ時のハンドラを設定して待機する
