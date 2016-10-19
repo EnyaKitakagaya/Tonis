c67dec8454adbdc7f574605a09326c6bdb76ae10
Penkich <nishijim2001@yahoo.co.jp>
2016-10-19 10:14:07 +0900

    Create activate_pre.php

---
+++ notePC/activate_pre.php
@@ -0,0 +1,9 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<h1>カードの有効化</h1>
+
+<p>有効化しようとするカードをかざして、確認ボタンを押してください。</p>
+<form action="activate.php" method="post">
+<input type="submit" value="確認">
+</form>
+</html>
