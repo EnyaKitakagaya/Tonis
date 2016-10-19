8763ddda5b8c356ed3d045014b66c8960ec60b63
Penkich <nishijim2001@yahoo.co.jp>
2016-10-19 09:52:15 +0900

    Create index.php

---
+++ notePC/index.php
@@ -0,0 +1,15 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<h1>メニュー</h1>
+<body>
+<p>
+  <a href="show_pre.php">カード情報の表示</a>
+</p>
+<p>
+  <a href="new.php">新規カード作成＋会員登録</a>
+</p>
+<p>
+  <a href="activate_pre.php">番頭によるカードの有効化（＋資格設定）</a>
+</p> 
+</body>
+</html>
