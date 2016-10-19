6cd84d8b5147b3fdd38d2091345b2ef6be9d9d6f
Penkich <nishijim2001@yahoo.co.jp>
2016-10-19 10:01:57 +0900

    Update show.php

--- notePC/show.php
+++ notePC/show.php
@@ -1,40 +1,59 @@
 <html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <?php
 //
 // カードをかざして会員登録情報を表示するツール by 2016-10-16 penkich
 //
+   include "init.inc";
+   include "amazonrds.inc";
 
-include "init.inc";
+   $sql = "select * from card where cardid = '$nfcid'";
+   $stmt = $dbh->query($sql);
+   $result = $stmt->fetch(PDO::FETCH_ASSOC);
+   $userid = $result['userid'];
+   $validity = $result['validity'];
+   $card_created_at = $result['created_at'];
+   $card_by_id = $result['by_id'];
 
-$sql = "select userid from card where cardid = '$nfcid'";
-$stmt = $dbh->query($sql);
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-$userid = $result['userid'];
-$sql = "select * from kaiin where userid = '$userid'";
-$stmt = $dbh->query($sql);
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
+   $sql = "select * from kaiin where userid = '$userid'";
+   $stmt = $dbh->query($sql);
+   $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$sei = $result['sei'];
-$mei = $result['mei'];
-$penname = $result['penname'];   
-$mail = $result['mail'];
-$tel = $result['tel'];
-$age = $result['age'];
-$sex = $result['sex'];
-$birthy = $result['birthy'];
-$place = $result['place'];
-$postal = $result['postal'];   
-
-print "会員番号:" . $userid . "<br>";
-print "姓：" . $sei . "<br>";
-print "名：" . $mei . "<br>";
-print "ハンドル名：" . $penname . "<br>";
-print "メール：" . $mail . "<br>";
-print "TEL：" . $tel . "<br>";
-print "年齢：" . $age . "<br>";
-print "性別：" . $sex . "<br>";
-print "誕生年：" . $birthy . "<br>";
-print "住所：" . $place . "<br>";
-print "郵便番号：" . $postal . "<br>";
+   $sei = $result['sei'];
+   $mei = $result['mei'];
+   $penname = $result['penname'];   
+   $mail = $result['mail'];
+   $tel = $result['tel'];
+   $age = $result['age'];
+   $sex = $result['sex'];
+   $birthy = $result['birthy'];
+   $place = $result['place'];
+   $postal = $result['postal'];
+   $by_id = $result['by_id'];
+   $mibun = $result['mibun'];
+   $created_at = $result['created_at'];
+   $by_id = $result['by_id'];
 ?>
+<body>
+<?php
+   print "<p>カード作成日:" . $card_created_at . "</P>";     
+   print "<p>有効性：" . $validity . "</p>";
+   print "<p>カード作成番頭：" . $card_by_id . "</p>";
+   print "<p>会員番号:" . $userid . "</P>";
+   print "<p>会員登録日:" . $created_at . "</P>";   
+   print "<p>姓：" . $sei . "</p>";
+   print "<p>名：" . $mei . "</p>";
+   print "<p>ハンドル名：" . $penname . "</p>";
+   print "<p>メール：" . $mail . "</p>";
+   print "<p>TEL：" . $tel . "</p>";
+   print "<p>性別：" . $sex . "</p>";
+   print "<p>誕生年：" . $birthy . "</p>";
+   print "<p>住所：" . $place . "</p>";
+   print "<p>郵便番号：" . $postal . "</p>";
+   print "<p>対応番頭：" . $by_id . "</p>";
+   print "<p>身分：" . $mibun . "</p>";
+
+?>
+<a href=index.php>メニューに戻る</a>
+</body>
 </html>
