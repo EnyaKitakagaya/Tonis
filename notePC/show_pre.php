87bbf061e3cde5dcf71e3fb887f1c68d3c84945c
Penkich <nishijim2001@yahoo.co.jp>
2016-10-19 10:04:59 +0900

    Create show_pre.php

---
+++ notePC/show_pre.php
@@ -0,0 +1,10 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<h1>カード情報の表示</h1>
+<body>
+<p>表示するカードをかざして、確認ボタンを押してください。</p>
+<form action="show.php" method="post">
+<input type="submit" value="表示">
+</form>
+</body>
+</html>
