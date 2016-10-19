58248f898c72432c0fbf1c95190e2d6a35851193
Penkich <nishijim2001@yahoo.co.jp>
2016-10-19 10:11:42 +0900

    Create act2.php

---
+++ notePC/act2.php
@@ -0,0 +1,40 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<?php
+
+   include "init.inc";
+   include "amazonrds.inc";
+   
+function mk_number(){
+    $s =  date('y') . sprintf("%03d",rand(1,999));
+    $chk = ($s[0]*6 + $s[1]*5 + $s[2]*4 + $s[3]*3 + $s[4]*2)%10;
+    return ($s . $chk);
+}
+
+$sql = "select count(*) as count from card where cardid = '$nfcid'";
+$stmt = $dbh->query($sql);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+$count = $result['count'];
+if ($count){
+    print "このカードは既に登録されています。<br>";
+    print "<a href=index.php>メニューに戻る</a>";
+    exit;
+}
+
+do {
+    $userid = mk_number();
+    $sql = "select count(*) as count from kaiin where userid = '$userid'";
+    $stmt = $dbh->query($sql);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $count = $result['count'];
+} while($count);
+
+
+$sql = "insert into card(cardid,userid) values('$nfcid','$userid')";
+$dbh->exec($sql);
+$sql = "insert into kaiin(userid,sei,mei,penname,mail,tel,age,sex,birthy,place,postal) values('$userid','$sei','$mei','$penname','$mail','$tel','$age','$sex','$birthy','$place','$postal')";
+$dbh->exec($sql);
+print "登録しました。このPCを番頭さんへ渡してください。";
+print "<a href=index.php>メニューに戻る</a>";
+?>
+</html>
